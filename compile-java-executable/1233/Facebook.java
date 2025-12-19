34bf6a051df6bb1fcee5bb27094866277633553a
TestGrid <info@testgrid.io>
2025-12-19 06:38:54 +0530

    Test Function Updated

--- compile-java-executable/1233/Facebook.java
+++ compile-java-executable/1233/Facebook.java
@@ -48,4 +48,15 @@ public class Facebook {
         tg.typeEncrypted("ele_password962", "ztDWA1mKeqUNzthlctyj9Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
         tg.close();
     }
+
+    public static void testcopy() {
+        tg.wait("ele_emailaddre934", ComparisonType.IS_VISIBLE);
+        tg.click("ele_emailaddre934", 1);
+        tg.wait("ele_emailaddre934", ComparisonType.IS_VISIBLE);
+        tg.type("ele_emailaddre934", "test");
+        tg.wait("ele_password962", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password962", 1);
+        tg.wait("ele_password962", ComparisonType.IS_VISIBLE);
+        tg.typeEncrypted("ele_password962", "ztDWA1mKeqUNzthlctyj9Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+    }
 }
