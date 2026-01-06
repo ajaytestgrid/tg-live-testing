53c295bc0bf1e1a89774245fe3ce442773cc982d
TestGrid <info@testgrid.io>
2026-01-07 05:22:40 +0530

    Test Case TDP Data Added

---
+++ compile-java-executable/1103/testcases/autosave/autosave_facebook.java
@@ -0,0 +1,18 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class facebook {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void facebook() {
+		tg.openBrowser();
+		tg.close();
+	}
+}
