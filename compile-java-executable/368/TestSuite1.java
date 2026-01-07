97c3979deca10de54f73f35b09f17d849af87f7d
ajaytestgrid <ajaytestgrid@testgrid.com>
2026-01-07 20:30:12 +0530

    Test Case Updated

--- compile-java-executable/368/TestSuite1.java
+++ compile-java-executable/368/TestSuite1.java
@@ -1,3 +1,31 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 @Listeners(TestListener.class)
 public class TestSuite1 {
+
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void facebook() {
+        tg.openBrowser();
+        tg.wait("ele_facebook168", ComparisonType.IS_VISIBLE);
+        tg.click("ele_facebook168", 1);
+        tg.close();
+    }
 }
